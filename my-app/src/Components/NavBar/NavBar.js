815f7c4fd3652348211b01b93a4290be22e7e9bb
Reality Audio Studio Dev <87328737+realityaudiostudio@users.noreply.github.com>
2023-03-12 15:48:58 +0530

    Add files via upload

---
+++ my-app/src/Components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <div className='navbar'>
+        <img className='mlicon' src="https://realityaudiostudio.github.io/img/icon.png" alt="ico" />
+        <img className='mlogo' src="https://realityaudiostudio.github.io/img/logo.svg" alt="logo" />
+    </div>
+  )
+}
+
+export default NavBar
